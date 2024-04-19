192fc776c5315c7d5de82f3e2c5699127b2ffeac
Sinan <muhammedsinan7749@gmail.com>
2024-04-19 10:35:05 +0530

    ;dfsdf

--- hashTable.js
+++ hashTable.js
@@ -56,13 +56,6 @@ class HashTable {
     }
 }
 
-// Example usage:
-const table = new HashTable();
-table.set('me', 'sinan');
-table.set('aou', 'love');
-table.display();
-
-
 
 
 
@@ -70,26 +63,60 @@ table.display();
 
 
 class Hash {
-    constructor(size = 32) {
-        this.size = size
-        this.table = new Array(size)
+    constructor(size = 54) {
+        this.size = size;
+        this.table = new Array(size);
     }
-    hash(key) {
-        let total = 0
-        for (let i = 0; i < key.length; i++) {
-            total = (total * 32 + key.charCodeAt(i)) % this.size
 
+    #hash(key) {
+        let total = 0;
+        for (let i = 0; i < key.length; i++) {
+            total = (total * 31 + key.charCodeAt(i)) % this.size;
         }
-        return total
+        return total;
     }
-    set(key, val) {
-        let index = this.hash(key)
+
+    set(key, value) {
+        let index = this.#hash(key);
         while (this.table[index] !== undefined && this.table[index].key !== key) {
-            index = (index + 1) % this.size
+            index = (index + 1) % this.size;
         }
-        this.table[index] = {key,val}
+        this.table[index] = { key, value };
     }
-    get(key){
-        let index = this.hash(key)
+
+    get(key) {
+        let index = this.#hash(key);
+        while (this.table[index] !== undefined) {
+            if (this.table[index].key === key) {
+                return this.table[index].value;
+            }
+            index = (index + 1) % this.size;
+        }
+        return undefined;
+    }
+
+    remove(key) {
+        let index = this.#hash(key);
+        while (this.table[index] !== undefined) {
+            if (this.table[index].key === key) {
+                this.table[index] = undefined;
+                return;
+            }
+            index = (index + 1) % this.size;
+        }
+    }
+
+    display() {
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i] !== undefined) {
+                console.log(`Key ${this.table[i].key} Value ${this.table[i].value}`);
+            }
+        }
     }
 }
+
+const hs = new Hash();
+hs.set('name', "Sinan");
+hs.set('Age', 18);
+console.log(hs.get('name')); // Output: Sinan
+hs.display();
